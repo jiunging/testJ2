ed784b71c742a8107a570ad2bf72371efbd48d0c
jiunging <hjs3080@naver.com>
2024-03-12 18:06:28 +0900

    test1

---
+++ testj2/src/main/java/com/example/testj2/Testj2Application.java
@@ -0,0 +1,13 @@
+package com.example.testj2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testj2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Testj2Application.class, args);
+	}
+
+}
